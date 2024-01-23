6bf23d83031137d4a8be210c7851d2c6333fac43
1Lucas1apk <76886832+1Lucas1apk@users.noreply.github.com>
2024-01-23 17:49:29 +0000

    Variable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello World!")
+    println!("Hello World!");
+
+	  // variavel 
+
+	  let name: &str = "Lucas";
+	  println!("Meu nome é {}", name);
+	
+	 // variável é imutável
+   // name = "Marcos"; (não é possível!)
+
+ 	// let age = 17;
+	// age += 1; (erro porque é umutavel);
+
+	// tranformar a variavel em mutável
+
+  let mut age: i32 = 17;
+	age += 1;
+	println!("Irei fazer {} anos", age);
+
+	// assim trabforma uma variável imutável em mutável;
 }
